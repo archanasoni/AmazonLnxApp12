5b4ce6c87a80397bc50518710c2b157ddf5246e0
Archana Soni <asoni@rocketsoftware.com>
2023-09-27 16:45:59 +0530

    First set of changes

---
+++ AmazonLnxApp1/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using IBM.Data.Db2;
+
+Console.WriteLine("Hello, World!");
+DB2Connection con = new DB2Connection();
+con.ConnectionString = Environment.GetEnvironmentVariable("connectionstring"); ;
+con.Open();
+Console.WriteLine("Connection opened");
